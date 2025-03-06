e3c0600498fd699eb3da730c9be38104286ff0bf
Santiago Alvarez <elsan.sa37@gmail.com>
2025-03-05 23:18:45 -0500

    Add vite requirements for react to project

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,1 +1,17 @@
-import './bootstrap';
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+
+  resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
+
+  setup({ el, App, props, }) {
+
+    createApp({ render: () => h(App, props) })
+
+
+
+  },
+
+});
